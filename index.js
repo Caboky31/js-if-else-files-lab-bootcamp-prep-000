fd386af4732e9d835bce14226a25d57446718a9d
caboky31@hotmail.com Bernard <caboky31@hotmail.com>
2018-09-04 18:58:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var height = "74"
 if (nmae === "Susan") {
   console.log("The name is correct")
 } else {
-  console.log("Expected teh name to be Susan")
+  console.log("Expected the name to be Susan")
 }
 
 // Don't worry about this
